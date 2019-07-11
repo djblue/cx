6c209770d31f6f54e865fdc7aa1178ae26a5c252
Chris Badahdah <a.s.badahdah@gmail.com>
2019-07-10 17:30:28 -0700

    Cleaup

--- build.cljs
+++
@@ -1,8 +0,0 @@
-(require '[lumo.build.api :as lumo])
-
-(lumo/build "src"
-  {:target :nodejs
-   :main 'ddev.main
-   :output-to "target/main.js"
-   :output-dir "target/lumo"
-   :optimizations :simple})
--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-trap 'kill $(jobs -p)' EXIT
-npm start > /dev/null 2>&1 &
-vim src/ddev/core.cljs
